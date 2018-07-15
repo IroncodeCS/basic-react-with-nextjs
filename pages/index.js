61d727638926306b2a846084ea445d15fd4a8371
Sarayut Khamkhiao <sarayut@thinknet.co.th>
2018-07-15 14:58:04 +0700

    add page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react'
+import Link from 'next/link'
 
-class index extends Component {
-    render(){
-        return(
-            <div>
-                <h1>Hello !! </h1>
-            </div>
-        )
-    }
-}
-export default index;
+export default () => (
+    <div>
+        <h1>Hello !! </h1>
+        <h2> Hi!! </h2>
+        <Link href='/page2' > <a>Page 2</a> </Link>
+        <Link href='/page3' > <a>Page 3</a> </Link>
+    </div>
+)
---
+++ pages/page2.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class page2 extends Component {
+    render(){
+        return(
+            <div>
+                <h1>Hello !! </h1>
+                <Link href='/page2' > <h2>Page 2</h2> </Link>
+                <Link href='/page3' > <h2>Page 3</h2> </Link>
+            </div>
+        )
+    }
+}
+export default index;
